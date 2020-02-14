0b59751e43709e7712b54164b449a6628dfb3883
Narsimha N <narayankhedkarn19@students.ecu.edu>
2020-02-14 11:13:03 -0500

    test

--- python-apps/wikipedia-api-app/WikiSearch.py
+++ python-apps/wikipedia-api-app/WikiSearch.py
@@ -6,7 +6,7 @@ def wiki_search(arg=None):
 		if arg:
 			# access the wikipedia api, first look for suggestions, then get
 			# the page
-			suggestion = wikipedia.search(arg, results=1, suggestion=True)
+			suggestion = wikipedia.search(arg, results=3, suggestion=True)
 			
 			if len(suggestion[0]) > 0:
 				page = wikipedia.page(suggestion[0][0])
@@ -18,10 +18,14 @@ def wiki_search(arg=None):
 				#print(summary)
 			return page.url
 		else:
+			summary = wikipedia.summary(arg, sentences=2)
+			print(summary)
 			return 'https://en.wikipedia.org/wiki/Main_Page'
 	except:
+		summary = wikipedia.search(arg)
+		print(summary)
 		return 'https://en.wikipedia.org/wiki/Main_Page'
 
-print(wiki_search("einstein"))
+print(wiki_search("Albert"))
 
 #print(wikipedia.summary("google", sentences=2))
