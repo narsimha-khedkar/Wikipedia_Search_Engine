5e194489473bc7c7352f4a63e6a1d12ba6a1c407
Narsimha N <narayankhedkarn19@students.ecu.edu>
2020-02-13 21:21:05 -0500

    Chapter 3

--- python-apps/wikipedia-api-app/chapter3.py
+++ python-apps/wikipedia-api-app/chapter3.py
@@ -4,12 +4,31 @@ https://pypi.org/project/Fuzzy/
 
 Levenshtein 
 https://pypi.org/project/python-Levenshtein/
-'''
-
 
 import Levenshtein
 
 distance('Levenshtein', 'Lenvinsten')
+
+'''
+
+
+#from Levenshtein import process
+
+from fuzzywuzzy import process
+
+with open("output.txt","r") as file:
+    terms = file.read().split("\n")
+
+total = len(terms)
+print("Number of Terms: ", str(total))
+
+def get_matches(word, possibilities, limit =3):
+        results = process.extract(word, possibilities, limit=limit)
+        return results
+    
+print(get_matches("Mustard", terms))
+
+
 
 
 
