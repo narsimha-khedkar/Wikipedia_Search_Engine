163a291531f9ff54aea134cf5e1b06e528389662
Narsimha N <narayankhedkarn19@students.ecu.edu>
2020-02-20 21:06:46 -0500

    Indexer

--- python-apps/wikipedia-api-app/indexer2.py
+++ python-apps/wikipedia-api-app/indexer2.py
@@ -1,32 +1,32 @@
-# /usr/bin/env python3
+
 import sys
 import re
 from collections import defaultdict
 
 
 def main(fh):
+    print("Processing Index")
     results = defaultdict(set)
 
-    for idx, line in enumerate(fh, start=1):
+    for index, line in enumerate(fh, start=1):
         words = re.findall(r'\w+', line)
         for word in words:
-            results[word].add(idx)
+            results[word].add(index)
 
     return results
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':        
     try:
-        in_file, out_file = "570.txt", "index.idx"
+        inputFile, outputFile = "570.txt", "index.idx"
     except IndexError:
-        print("Usage:\n{} <in_file> <out_file>".format(sys.argv[0]),
-              file=sys.stderr)
-        exit(2)
+        print("File not found")
+        exit(1)
 
-    with open(in_file) as input_file:
+    with open(inputFile) as input_file:
         index = main(input_file)
 
-    with open(out_file, 'w') as out_fh:
+    with open(outputFile, 'w') as out_fh:
         for word in sorted(index):
             print(word + " " +  " ".join(str(i) for i in sorted(index[word])),
                   file=out_fh)
