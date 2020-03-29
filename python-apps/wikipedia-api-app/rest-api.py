0cfb8487da1b0060357f4169ab7bb73b2350cb41
Thomas James Barnhouse <barnhouset09@gmail.com>
2020-03-29 19:40:17 -0400

    showing sentences from text on report page now

--- python-apps/wikipedia-api-app/rest-api.py
+++ python-apps/wikipedia-api-app/rest-api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
+import json
 from json import dumps, JSONEncoder
 from flask_jsonpify import jsonify, jsonpify
 from flask_cors import CORS
@@ -59,14 +60,16 @@ def get_article(articleName):
 def get_article_data(articleName):
     if request.method == 'GET':
         try:
-            article =  wikipedia.page(articleName).content
-            encodedArticle = MyEncoder().encode(article)
+            article =  wikipedia.page(articleName)
+            articleContent = article.content
+            encodedArticle = MyEncoder().encode(articleContent)
             return encodedArticle
         except wikipedia.DisambiguationError as e:
             # occurs when the wikipedia search returns a disambiguation page (a page with a list of multiple related articles)
             # capture this, then return one of them. Might offer some special functionality later on to allow the user to choose
             firstOption = e.options[0]
-            nonAmbiguationArticle = wikipedia.page(firstOption).content
+            entireArticle = wikipedia.page(firstOption)
+            nonAmbiguationArticle = entireArticle.content
             encodedArticle = MyEncoder().encode(nonAmbiguationArticle)
             return encodedArticle
         except Exception as e:
@@ -78,12 +81,20 @@ def get_article_data(articleName):
 def generatereport():
     if request.method == 'POST':
         try:
-            articleData = request.form['articleData']
+            articleDataJson = request.form['articleData']
+            articleData = json.loads(articleDataJson)
             queryString = request.form['queryString']
 
+            # if "articleData" in request.form.fromkeys
+            #     articleData = request.form['articleData']
+
+            # if "queryString" in request.form
+            #     queryString = request.form['queryString']
+  
+        
             file = request.files['pdfFile']
             fileName = secure_filename(file.filename)
-            file.save(fileName)
+            file.save(fileName) 
 
             # extract and sanitize the text
             # this takes forever for textbook files!! we should probably think about storing the text somehow
@@ -93,18 +104,17 @@ def generatereport():
             text = text.replace('\n','').replace('\r','').lower()
             transTable = [string.punctuation,""]
             sanitizedText = text.translate(transTable)
+            joinedArticleText = ' '.join(articleData)
+            articleText = joinedArticleText.replace('\n','').replace('\r','').lower()
+            sanitizedArticleText = articleText.translate(transTable)
+            articleSentences = nltk.sent_tokenize(sanitizedArticleText)
             sentences = nltk.sent_tokenize(sanitizedText)
-            words = nltk.word_tokenize(sanitizedText)
-            
-            index = InvertedIndex(nltk.word_tokenize, EnglishStemmer(), nltk.corpus.stopwords.words('english'))
-            
-            for sentence in [s for s in sentences]:
-                index.add(sentence)
+            sentences.extend(articleSentences)
 
-            # testing out how we could perform the query on the text. the nltk is going to be super useful in this space
-            testQueryResult = index.lookup('inverted')
-
-            return MyEncoder().encode("File received on API server.")
+            # words = nltk.word_tokenize(sanitizedText)
+            
+            return MyEncoder().encode(sentences)
+            
         except Exception as e:
             print("Unexpected error", e)
             return e
